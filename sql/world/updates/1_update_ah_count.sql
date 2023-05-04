430cc2f37e1727c1718154c17e751e2f62c62f63
jshplayer <jshplayer@outlook.com>
2023-05-05 07:31:24 +1200

    Setting Oceanic War Limits

---
+++ sql/world/updates/1_update_ah_count.sql
@@ -0,0 +1,3 @@
+UPDATE `acore_world`.`mod_auctionhousebot` SET `minitems` = '0', `maxitems` = '10000', `maxstackgrey` = '10', `maxstackwhite` = '5' WHERE (`auctionhouse` = '7');
+UPDATE `acore_world`.`mod_auctionhousebot` SET `minitems` = '0', `maxitems` = '0', `maxstackgrey` = '10', `maxstackwhite` = '5' WHERE (`auctionhouse` = '6');
+UPDATE `acore_world`.`mod_auctionhousebot` SET `minitems` = '0', `maxitems` = '0', `maxstackgrey` = '10', `maxstackwhite` = '5' WHERE (`auctionhouse` = '2');
